69439be91fbf8327e32232d92d3160416dc6062f
Rémi Le Guin <remileguin@live.fr>
2021-03-04 12:01:33 +0100

    Preparation to deploy on Heroku

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,11 @@
-
 import { createElement } from 'lwc';
 import MyApp from 'my/app';
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js');
+    });
+}
 
 const app = createElement('my-app', { is: MyApp });
 // eslint-disable-next-line @lwc/lwc/no-document-query
