fad891bda8ad5dee98c96c3b694cb6995244bd14
Rémi Le Guin <remileguin@live.fr>
2021-03-08 23:00:01 +0100

    Like buttons

--- src/client/modules/recordshop/home/home.js
+++ src/client/modules/recordshop/home/home.js
@@ -4,12 +4,10 @@ import { getRecords } from 'data/recordService';
 export default class Home extends LightningElement {
     allRecords = [];
     popularRecords = [];
-    otherRecords = [];
     connectedCallback() {
         getRecords().then((result) => {
             this.allRecords = result;
             this.popularRecords = result.slice(0, 6);
-            this.otherRecords = result.slice(6)
         });
     }
 }
