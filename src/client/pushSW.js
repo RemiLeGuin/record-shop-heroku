81b41115a3a9978f42bbf938193e3fdfd30573b3
Rémi Le Guin <remileguin@live.fr>
2021-03-16 14:12:46 +0100

    pushSW showNotification handling

--- src/client/pushSW.js
+++ src/client/pushSW.js
@@ -1,6 +1,6 @@
 self.addEventListener('push', (event) => {
-    const body = event.data ? event.data.text() : 'no payload';
+    const data = event.data ? JSON.parse(event.data.text()) : 'No content';
     event.waitUntil(
-        self.registration.showNotification('LWC Push Notifications PWA', { body })
+        self.registration.showNotification(data.title, { body: data.body, image: data.image, icon: data.icon, badge: data.badge })
     );
 });
