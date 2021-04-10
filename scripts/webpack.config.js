b897a1c2146101a3840cacb5b75191ff8e382482
Rémi Le Guin <remileguin@live.fr>
2021-04-10 19:30:37 +0200

    Cache

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -1,7 +1,7 @@
 // Custom webpack configuration file, provides generation of service worker
 // More information: https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
 const { GenerateSW } = require('workbox-webpack-plugin');
-//const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     plugins: [
